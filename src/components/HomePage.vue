23b82a299154166029c6836844f41da2d380dd5f
Arvin <arvincris@gmail.com>
2024-05-18 16:59:57 +0800

    HTML CSS Gallery Design

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,9 +2,10 @@
 </script>
 
 <template>
-    <nav>
+    <nav id="nav">
         <div class="navTop">
             <div class="navItem">
+                <img src="/src/assets/images/vue.png" alt="" class="logoTitle">
                 <img src="/src/assets/images/niek-logo.png" alt="">
             </div>
             <div class="navItem">
@@ -160,6 +161,36 @@
             </div>
             <button class="payButton">Checkout</button>
             <span class="close">X</span>
+        </div>
+    </div>
+    <div class="gallery">
+        <div class="galleryItem">
+            <h1 class="galleryTitle">Step into greatness!</h1>
+            <img src="https://i.pinimg.com/originals/05/67/98/0567986f7aea439bfa0706ff4b3e2f2a.jpg" class="galleryImg">
+        </div>
+        <div class="galleryItem">
+            <img src="https://i.pinimg.com/originals/68/aa/1e/68aa1eb3ce38fee2aadd05568e343b1d.png" class="galleryImg">
+            <h1 class="galleryTitle">Be Yourself!</h1>
+        </div>
+        <div class="galleryItem">
+            <h1 class="galleryTitle">Comfort and style!</h1>
+            <img src="https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/dca799ba-673e-4ede-bebf-c14021a7e547/air-max-flyknit-racer-shoes-tTv54H.png" class="galleryImg">
+        </div>
+    </div>
+    <div class="newSeason">
+        <div class="nsItem">
+            <img src="https://images.squarespace-cdn.com/content/v1/55665947e4b091320576d67c/1486418043130-7JF20J9EHK9FCRH5X9B5/image-asset.jpeg" alt="" class="nsImg">
+        </div>
+        <div class="nsItem">
+            <h3 class="nsTitleSm">Winter New Arrivals</h3>
+            <h1 class="nsTitle">New Season</h1>
+            <h1 class="nsTitle">New Collection</h1>
+            <a href="#nav">
+                <button class="nsButton">CHOOSE YOUR STYLE</button>
+            </a>
+        </div>
+        <div class="nsItem">
+            <img src="https://sneakernews.com/wp-content/uploads/2019/10/nike-gear-up-1.jpg" alt="" class="nsImg">
         </div>
     </div>
 </template>
