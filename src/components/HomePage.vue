3b85b3ea8c1a21517f25f5603bd591b726871da6
Arvin <arvincris@gmail.com>
2024-05-20 10:00:42 +0800

    Adding of footer styles

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -179,7 +179,7 @@
     </div>
     <div class="newSeason">
         <div class="nsItem">
-            <img src="https://images.squarespace-cdn.com/content/v1/55665947e4b091320576d67c/1486418043130-7JF20J9EHK9FCRH5X9B5/image-asset.jpeg" alt="" class="nsImg">
+            <img src="https://sneakernews.com/wp-content/uploads/2019/10/nike-gear-up-1.jpg" alt="" class="nsImg">
         </div>
         <div class="nsItem">
             <h3 class="nsTitleSm">Winter New Arrivals</h3>
@@ -190,7 +190,62 @@
             </a>
         </div>
         <div class="nsItem">
-            <img src="https://sneakernews.com/wp-content/uploads/2019/10/nike-gear-up-1.jpg" alt="" class="nsImg">
+            <img src="https://images.squarespace-cdn.com/content/v1/55665947e4b091320576d67c/1486418043130-7JF20J9EHK9FCRH5X9B5/image-asset.jpeg" alt="" class="nsImg">
         </div>
     </div>
+    <footer>
+        <div class="footerLeft">
+            <div class="footerMenu">
+                <h1 class="fMenuTitle">About Us</h1>
+                <ul class="fList">
+                    <li class="fListItem">Company</li>
+                    <li class="fListItem">Contact</li>
+                    <li class="fListItem">Careers</li>
+                    <li class="fListItem">Affiliates</li>
+                    <li class="fListItem">Stores</li>
+                </ul>
+            </div>
+            <div class="footerMenu">
+                <h1 class="fMenuTitle">Useful Links</h1>
+                <ul class="fList">
+                    <li class="fListItem">Support</li>
+                    <li class="fListItem">Refund</li>
+                    <li class="fListItem">FAQ</li>
+                    <li class="fListItem">Feedback</li>
+                    <li class="fListItem">Stories</li>
+                </ul>
+            </div>
+            <div class="footerMenu">
+                <h1 class="fMenuTitle">Products</h1>
+                <ul class="fList">
+                    <li class="fListItem">Air Max</li>
+                    <li class="fListItem">Air Jordan</li>
+                    <li class="fListItem">Blazer</li>
+                    <li class="fListItem">Crater</li>
+                    <li class="fListItem">Hippie</li>
+                </ul>
+            </div>
+        </div>
+        <div class="footerRight">
+            <div class="footerRightMenu">
+                <h1 class="fMenuTitle">Subscribe to our Newsletter</h1>
+                <div class="fMail">
+                    <input type="text" placeholder="your@email.com" class="fInput">
+                    <button class="fButton">Join!</button>
+                </div>
+            </div>
+            <div class="footerRightMenu">
+                <h1 class="fMenuTitle">Follow Us</h1>
+                <div class="fIcons">
+                    <img src="/src/assets/images/facebook.png" alt="" class="fIcon">
+                    <img src="/src/assets/images/twitter.png" alt="" class="fIcon">
+                    <img src="/src/assets/images/instagram.png" alt="" class="fIcon">
+                    <img src="/src/assets/images/whatsapp.png" alt="" class="fIcon">
+                </div>
+            </div>
+            <div class="footerRightMenu">
+                <span class="copyright">@arvincris97. All rights reserved. 2024</span>
+            </div>
+        </div>
+    </footer>
 </template>
